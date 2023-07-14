3ebbebfa6330ec30b5a6fa1aaa1f19ad3c49c8f3
Todd Barrett <toddcbarrett3@gmail.com>
2023-07-14 07:08:23 -0700

    Completed Exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalidrome(string input) 
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray) == input;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("radar", true)]
+        [InlineData("racecar", true)]
+        [InlineData("hiking", false)]
+        [InlineData("fishing", false)]
+        public void IsAPalindromeTest(string input, bool expected)
         {
+            WordSmith palindrome = new WordSmith();
+            
+            var actual = palindrome.IsAPalidrome(input);
 
+            Assert.Equal(expected, actual);
         }
     }
 }
